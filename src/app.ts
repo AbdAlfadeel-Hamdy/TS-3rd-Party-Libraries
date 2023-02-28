564390227610a14f545ed76617257724d6489e86
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-02-28 21:16:53 +0200

    using class validator package

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import _ from "lodash";
 import "reflect-metadata";
 import { plainToInstance } from "class-transformer";
+import { validate } from "class-validator";
 import { Product } from "./product.model";
 
 console.log(_.shuffle([1, 3, 2, 4]));
@@ -31,3 +32,16 @@ const loadedProducts = plainToInstance(Product, products);
 
 for (const product of loadedProducts) console.log(product.getInformation());
 console.log(p1.getInformation());
+
+/// Class Validator
+
+const newProduct = new Product("", -5);
+
+validate(newProduct).then((errors) => {
+  if (errors.length > 0) {
+    console.log("Validation errors!");
+    console.log(errors);
+  } else {
+    console.log(newProduct.getInformation());
+  }
+});
